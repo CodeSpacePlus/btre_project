ecef1cc4102e2301141cb2765506b86feb098577
CodeSpacePlus <bryanc0791@gmail.com>
2020-05-27 14:13:31 -0700

    Finished realtors models and added to admin page

--- realtors/models.py
+++ realtors/models.py
@@ -1,5 +1,15 @@
 from django.db import models
+from datetime import datetime
 
 
 class Realtor(models.Model):
-  name = models.CharField(max_length=50)
+  name = models.CharField(max_length=100)
+  description = models.TextField(blank=True)
+  email = models.EmailField()
+  phone = models.CharField(max_length=30)
+  is_mvp = models.BooleanField(default=False)
+  hire_date = models.DateTimeField(default=datetime.now, blank=True)
+  photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+
+  def __str__(self):
+    return self.name
