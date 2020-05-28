9799757a00c16b8d059be13a12e68559173712bf
CodeSpacePlus <bryanc0791@gmail.com>
2020-05-28 13:24:12 -0700

    Finished pagination part of listings.html

--- listings/views.py
+++ listings/views.py
@@ -1,18 +1,24 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
 from .models import Listing
 
 
 def index(request):
-  listings = Listing.objects.all()
-  context = {
-    'listings': listings
-  }
-  return render(request, 'listings/listings.html', context)
+    listings = Listing.objects.all()
 
-def listing(request, id):
-  return render(request, 'listings/listing.html')
+    paginator = Paginator(listings, 3)
+    page_number = request.GET.get('page')
+    paged_listings = paginator.get_page(page_number)
+
+    context = {
+        'listings': paged_listings
+    }
+    return render(request, 'listings/listings.html', context)
+
+
+def listing(request, listing_id):
+    return render(request, 'listings/listing.html')
+
 
 def search(request):
-  return render(request, 'listings/search.html')
-
-
+    return render(request, 'listings/search.html')
