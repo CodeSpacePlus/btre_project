92254567bdda0c8192999ecd241ab3a9ac0936d0
CodeSpacePlus <bryanc0791@gmail.com>
2020-06-01 09:50:32 -0700

    Searches are no preserved after submitting.

--- listings/views.py
+++ listings/views.py
@@ -66,6 +66,7 @@ def search(request):
         'listings': paged_listings,
         'state_choices': state_choices,
         'bedroom_choices': bedroom_choices,
-        'price_choices': price_choices
+        'price_choices': price_choices,
+        'values': request.GET,
     }
     return render(request, 'listings/search.html', context)
